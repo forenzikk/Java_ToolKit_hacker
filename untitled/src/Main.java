1f294eb46dd8e37eafd63bc606a96f1ad614ac9f
art86 <sddivanov@yandex.ru>
2024-04-10 22:19:23 +0300

    addition

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, hacker!");
         System.out.println("Nice to meet you in my Hacker ToolKit!");
+        System.out.println("....");
     }
 }
